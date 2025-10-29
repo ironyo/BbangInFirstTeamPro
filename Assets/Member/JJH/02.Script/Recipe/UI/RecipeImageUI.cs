d1298a56b80fec4004624fd4e5ad7efd017195c7
ForRest1217 <Jihan2519@naver.com>
2025-10-29 17:17:57 +0900

    Debug

--- Assets/Member/JJH/02.Script/Recipe/UI/RecipeImageUI.cs
+++ Assets/Member/JJH/02.Script/Recipe/UI/RecipeImageUI.cs
@@ -1,6 +1,46 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class RecipeImageUI : MonoBehaviour
+public class RecipeImageUI : MonoBehaviour, IPointerClickHandler, ISelectHandler, IDeselectHandler
 {
+    [SerializeField] private Image image;
+    private GameObject describeObject;
+    private Outline outline;
 
+    private RecipeSO information;
+    private int foodAmount = 1;
+
+    public void Create(RecipeSO ingredient, int amount)
+    {
+        amountText = GetComponentInChildren<TextMeshProUGUI>();
+        outline = GetComponent<Outline>();
+
+        outline.enabled = false;
+
+        image.sprite = ingredient.foodImage;
+        information = ingredient;
+        foodAmount = amount;
+
+        amountText.text = foodAmount.ToString();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(gameObject);
+
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        outline.enabled = true;
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        outline.enabled = false;
+
+    }
 }
