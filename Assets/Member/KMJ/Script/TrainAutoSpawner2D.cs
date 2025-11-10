8d45756f0668a65eb217253c394e985bfdad9939
KMJ <relltyos12@gmail.com>
2025-11-10 09:55:32 +0900

    KMJ_Shotgun

--- Assets/Member/KMJ/Script/TrainAutoSpawner2D.cs
+++ Assets/Member/KMJ/Script/TrainAutoSpawner2D.cs
@@ -1,12 +1,14 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TrainAutoSpawner2D : MonoBehaviour
 {
     public CarHeadMovement2D headMovement;
     public TrainChainFollower2D chain;
     public Transform carPrefab;
-    public int carCount = 4;
+    public int carCount;
 
     private void Start()
     {
@@ -18,14 +20,23 @@ public class TrainAutoSpawner2D : MonoBehaviour
         chain.cars ??= new List<Transform>();
         chain.cars.Clear();
 
+        
+    }
+
+    private void Update()
+    {
         // 초기 배치: 헤드 뒤로 linkLength 간격만큼
         Vector3 basePos = headMovement.transform.position;
         Vector3 back = -headMovement.transform.up * chain.linkLength;
 
-        for (int i = 0; i < carCount; i++)
+        if (Keyboard.current.kKey.wasPressedThisFrame)
         {
-            Transform car = Instantiate(carPrefab, basePos + back * (i + 1), headMovement.transform.rotation);
-            chain.cars.Add(car);
+            Debug.Log("ddd");
+            for (int i = 0; i < carCount; i++)
+            {
+                Transform tail = Instantiate(carPrefab, basePos + back * (i + 1), headMovement.transform.rotation);
+                chain.cars.Add(tail);
+            }
         }
     }
 }
