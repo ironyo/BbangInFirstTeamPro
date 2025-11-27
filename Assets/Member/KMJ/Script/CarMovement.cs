862acc5a09cc3f9ce2c1e6c5a63d6f4ca8e68ec6
Valay0429 <a49986661@gmail.com>
2025-11-27 14:54:23 +0900

    minigame

--- Assets/Member/KMJ/Script/CarMovement.cs
+++ Assets/Member/KMJ/Script/CarMovement.cs
@@ -1,17 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class CarMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;       
-    [SerializeField] private float rotationSpeed = 5f;   
-    [SerializeField] private float acceleration = 2f;    
-    [SerializeField] private float deceleration = 3f;    
-
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 5f;
     private Rigidbody2D _rb;
     private Vector2 moveDir;
-    private float currentSpeed = 0f;                     
-    public bool canMove = false;
-    private bool isMoving = false;
 
     private void Awake()
     {
@@ -20,56 +14,31 @@ public class CarMovement : MonoBehaviour
 
     private void Update()
     {
-        if (!canMove)
-        {
-            moveDir = Vector2.zero;
-            isMoving = false;
-            return;
-        }
-
-        moveDir = Vector2.zero;
-        isMoving = false;
-    }
-
-    private void FixedUpdate()
-    {
         if (Input.GetKey(KeyCode.W))
         {
             moveDir = transform.up;
-            isMoving = true;
-
-            if (Input.GetKey(KeyCode.A))
-                transform.eulerAngles += new Vector3(0, 0, rotationSpeed);
-            if (Input.GetKey(KeyCode.D))
-                transform.eulerAngles += new Vector3(0, 0, -rotationSpeed);
         }
         else if (Input.GetKey(KeyCode.S))
         {
             moveDir = -transform.up;
-            isMoving = true;
-
-            if (Input.GetKey(KeyCode.A))
-                transform.eulerAngles += new Vector3(0, 0, -rotationSpeed);
-            if (Input.GetKey(KeyCode.D))
-                transform.eulerAngles += new Vector3(0, 0, rotationSpeed);
-        }
-
-        if (isMoving)
-        {
-            currentSpeed = Mathf.MoveTowards(currentSpeed, moveSpeed, acceleration * Time.fixedDeltaTime);
         }
         else
         {
-            currentSpeed = Mathf.MoveTowards(currentSpeed, 0f, deceleration * Time.fixedDeltaTime);
+            moveDir = Vector2.zero;
         }
+    }
 
-        
-        if (!canMove)
+    private void FixedUpdate()
+    {
+        _rb.linearVelocity = moveDir * moveSpeed * Time.fixedDeltaTime;
+
+        if (Input.GetKey(KeyCode.A))
         {
-            _rb.linearVelocity = Vector2.zero;
-            return;
+            transform.eulerAngles += new Vector3(0, 0, rotationSpeed);
         }
-
-        _rb.linearVelocity = moveDir * currentSpeed;
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.eulerAngles += new Vector3(0, 0, -rotationSpeed);
+        }
     }
 }
--- Assets/Member/KU/02.Script/Manager/AlbaManager.cs
+++ Assets/Member/KU/02.Script/Manager/AlbaManager.cs
@@ -2,30 +2,6 @@ using UnityEngine;
 
 public class AlbaManager : MonoBehaviour
 {
-    [SerializeField] private GameObject plusGroup;
-    [SerializeField] private GameObject minusGroup;
-    [SerializeField] private GameObject menuGroup;
-    public void OpenMenu(bool isPlus)
-    {
-        menuGroup.SetActive(false);
-        if (isPlus)
-        {
-            plusGroup.SetActive(true);
-            minusGroup.SetActive(false);
-        }
-        else
-        {
-            plusGroup.SetActive(false);
-            minusGroup.SetActive(true);
-        }
-    }
-    public void CloseAllMenu()
-    {
-        plusGroup.SetActive(false);
-        minusGroup.SetActive(false);
-        menuGroup.SetActive(true);
-    }
-
     public void GetxAlbaData()
     {
 
