e1ab9dbbdba46f394df232290a4da1c6594e43f5
ironyo <kyou09kang@gmail.com>
2025-11-29 23:54:58 +0900

    HatDogWeaponPlus01

--- Assets/Member/KU/02.Script/Tower/KU_Bullet.cs
+++ Assets/Member/KU/02.Script/Tower/KU_Bullet.cs
@@ -1,19 +1,31 @@
+using Cysharp.Threading.Tasks;
+using System.Threading;
 using UnityEngine;
 
 public class KU_Bullet : MonoBehaviour
 {
-    private BoxCollider2D _colliderCompo;
     private Rigidbody2D _rigidbodyCompo;
 
     private Vector2 _moveDir;
     [SerializeField] private float _moveSpeed;
+    [SerializeField] private GameObject _redSourcePref;
+    [SerializeField] private GameObject _yellowSourcePref;
+
+    private bool nowSourceFlag = true;
 
     private Vector3 rotationSpeed = new Vector3(0, 0, 10);
 
+    private CancellationTokenSource cancellationTokenSource;
+
+
     private void Awake()
     {
-        _colliderCompo = GetComponent<BoxCollider2D>();
         _rigidbodyCompo = GetComponent<Rigidbody2D>();
+        cancellationTokenSource = new CancellationTokenSource();
+    }
+    private void Start()
+    {
+        SpawnSource().Forget();
     }
 
     private void Update()
@@ -24,5 +36,45 @@ public class KU_Bullet : MonoBehaviour
     public void GetTarget(Transform pos)
     {
         _moveDir = pos.position - transform.position;
+    }
+
+    private async UniTask SpawnSource()
+    {
+        try
+        {
+            while (!cancellationTokenSource.IsCancellationRequested)
+            {
+                await UniTask.WaitForSeconds(0.3f, cancellationToken: cancellationTokenSource.Token);
+
+                if (this == null) return;
+                if (transform == null) return;
+
+                GameObject obj = Instantiate(
+                    nowSourceFlag ? _redSourcePref : _yellowSourcePref, 
+                    transform.position, 
+                    transform.rotation);
+
+                nowSourceFlag = !nowSourceFlag;
+
+                Rigidbody2D rb = obj.GetComponent<Rigidbody2D>();
+                rb.linearVelocity = transform.right * _moveSpeed;
+            }
+        }
+        catch (System.OperationCanceledException)
+        {
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<KU_Enemy>(out KU_Enemy enemy))
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnDestroy()
+    {
+        cancellationTokenSource?.Cancel();
     }
 }
--- Assets/Member/KU/02.Script/Tower/KU_Source.cs
+++ Assets/Member/KU/02.Script/Tower/KU_Source.cs
@@ -1,16 +1,29 @@
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class KU_Source : MonoBehaviour
 {
-    private Rigidbody2D _rigidCompo;
-
+    private CancellationTokenSource cancellationTokenSource;
+    private Rigidbody2D _rigidbodyCompo;
     private void Awake()
     {
-        _rigidCompo = GetComponent<Rigidbody2D>();
+        _rigidbodyCompo = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    private void Start()
     {
-        
+        DropSource().Forget();
+    }
+
+    private async UniTask DropSource()
+    {
+        await UniTask.WaitForSeconds(0.5f);
+        _rigidbodyCompo.linearVelocity = Vector2.zero;
+        transform.DOScale(new Vector3(2, 2, 2), 1f);
+        await UniTask.WaitForSeconds(3f);
+        Destroy(gameObject);
     }
 }
