26da5d859b40b87a7f3e84e05b262d4e1d61f076
ironyo <kyou09kang@gmail.com>
2025-11-30 00:41:51 +0900

    wq2e3refgbhnjjmm,kl

--- Assets/Member/KU/02.Script/Tower/KU_Source.cs
+++ Assets/Member/KU/02.Script/Tower/KU_Source.cs
@@ -1,5 +1,6 @@
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -8,6 +9,10 @@ public class KU_Source : MonoBehaviour
 {
     private CancellationTokenSource cancellationTokenSource;
     private Rigidbody2D _rigidbodyCompo;
+
+    private HashSet<KU_Enemy> insideEnemies = new HashSet<KU_Enemy>();
+    private Dictionary<KU_Enemy, CancellationTokenSource> enemyTokens = new Dictionary<KU_Enemy, CancellationTokenSource>();
+
     private void Awake()
     {
         _rigidbodyCompo = GetComponent<Rigidbody2D>();
@@ -25,5 +30,39 @@ public class KU_Source : MonoBehaviour
         transform.DOScale(new Vector3(2, 2, 2), 1f);
         await UniTask.WaitForSeconds(3f);
         Destroy(gameObject);
+    }
+
+    private async UniTask StartAttackLoop(KU_Enemy enemy, CancellationToken token)
+    {
+        while (!token.IsCancellationRequested)
+        {
+            await UniTask.WaitForSeconds(1f, cancellationToken: token);
+            Debug.Log($"Attack to {enemy.name}");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<KU_Enemy>(out KU_Enemy enemy))
+        {
+            if (insideEnemies.Add(enemy))
+            {
+                var cts = new CancellationTokenSource();
+                enemyTokens[enemy] = cts;
+                StartAttackLoop(enemy, cts.Token).Forget();
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<KU_Enemy>(out KU_Enemy enemy))
+        {
+            if (insideEnemies.Remove(enemy))
+            {
+                enemyTokens[enemy].Cancel();
+                enemyTokens.Remove(enemy);
+            }
+        }
     }
 }
