d452af341b3e3efb8ce9d64a98ae0ab194f2b0a6
ForRest1217 <Jihan2519@naver.com>
2025-12-04 17:22:43 +0900

    Pizza

--- Assets/Member/JJH/02.Script/Turret/Bullet/BulletMove.cs
+++ Assets/Member/JJH/02.Script/Turret/Bullet/BulletMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class BulletMove : MonoBehaviour
 {
     private Rigidbody2D rigid;
-    private float speed = 5;
+    [SerializeField] private float speed = 5;
     public float Speed
     {
         get
--- Assets/Member/JJH/02.Script/Turret/Bullet/Pizza/PizzaBullet.cs
+++ Assets/Member/JJH/02.Script/Turret/Bullet/Pizza/PizzaBullet.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PizzaBullet : IncreaseSpeed
 {
     [SerializeField] private GameObject childrenBullet;
+    [SerializeField] private GameObject tomatoSauce;
 
     private BulletMove bulletMove;
 
@@ -24,10 +25,20 @@ public class PizzaBullet : IncreaseSpeed
     {
         if (collision.CompareTag("Enemy"))
         {
+            float offset = 1.5f;
             for (int i = 0; i <= 360; i += 45)
             {
-                Instantiate(childrenBullet, transform.position, Quaternion.Euler(0, 0, i));
+                //각도를 라디안으로 변환
+                float radian = i * Mathf.Deg2Rad;
+                //벡터로 바꾸기
+                Vector3 dir = new Vector3(Mathf.Cos(radian), Mathf.Sin(radian), 0f);
+                // 현재 위치에 위치 +해주기
+                Vector3 spawnPos = transform.position + dir * offset;
+
+                Instantiate(childrenBullet, spawnPos, Quaternion.Euler(0, 0, i - 90));
+                Instantiate(tomatoSauce, transform.position, Quaternion.Euler(0, 0, i));
             }
+
             Destroy(gameObject);
         }
     }
