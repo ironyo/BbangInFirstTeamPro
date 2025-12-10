86482d47e776bad35af434e1f6d475875e71460b
ForRest1217 <Jihan2519@naver.com>
2025-12-11 00:15:26 +0900

    ToolTipShow

--- Assets/Member/JJH/02.Script/UI/ToolTipManager.cs
+++ Assets/Member/JJH/02.Script/UI/ToolTipManager.cs
@@ -1,21 +1,59 @@
-using TMPro;
-using UnityEngine.UI;
+using Assets.Member.CHG._02.Scripts.Pooling;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class ToolTipManager : MonoSingleton<ToolTipManager>
 {
-    private Image image;
-    private TextMeshProUGUI text;
+    [SerializeField] private ToolTipUI tooltipUIPrefab;
 
-    private void Awake()
+    Factory factory;
+
+    private readonly List<ToolTipUI> active = new List<ToolTipUI>();
+
+    private void Start()
     {
-        image = GetComponent<Image>();
-        text = GetComponentInChildren<TextMeshProUGUI>();
+        factory = new Factory(tooltipUIPrefab.gameObject, 6);
     }
 
     public void ShowToolTip(string text)
     {
-        this.text.text = text;
+        if (active.Count >= 3)
+        {
+            ToolTipUI oldest = active[0];
+            active.RemoveAt(0);
+            oldest.DestroyUI();
+        }
 
+        IRecycleObject tooltip = factory.Get();
+        tooltip.GameObject.transform.SetParent(transform, false);
+        ToolTipUI ToolTipUI = tooltip.GameObject.GetComponent<ToolTipUI>();
+        ToolTipUI.SetText(text);
+        active.Add(ToolTipUI);
+
+        ReMove();
+    }
+
+    private void ReMove()
+    {
+        StartCoroutine(DelayedReMove());
+    }
+
+    public IEnumerator DelayedReMove()
+    {
+        yield return new WaitForSeconds(0.05f);
+        for (int i = 0; i < active.Count; i++)
+        {
+            int index = active.Count - i - 1;
+            float y = new Vector3(0, -300, 0).y + (100f * index);
+            active[i].Move(new Vector3(0, y, 0));
+        }
+    }
+
+    public void RemoveActive(ToolTipUI toolTipUI)
+    {
+        active.Remove(toolTipUI);
+        ReMove();
     }
 
     public void HideToolTip()
