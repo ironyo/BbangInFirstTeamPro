e5853d81ca4a22574c4c6b9edf4a73cbe861dc62
Hongjihoo123 <hongjihoo123@naver.com>
2025-12-11 20:47:08 +0900

    CustomerParticle

--- Assets/Member/HJH/02.Scripts/Customer/State/AttackState.cs
+++ Assets/Member/HJH/02.Scripts/Customer/State/AttackState.cs
@@ -32,23 +32,27 @@ public class AttackState : IEnemyState
     {
         if (customer.customerHP <= 0)
         {
+            animator.SetBool("isAttack", false);
             customer.ChangeState(customer.ClearState);
             return;
         }
 
         if (customer.IsAttackTargetInRange() == false)
         {
+            animator.SetBool("isAttack", false);
             customer.ChangeState(customer.CloseState);
             return;
         }
 
-        if (!isAttacking)
+        if(customer.IsAttackTargetInRange() == true)
+        {
             AttackTween();
+        }
     }
 
     public void Exit()
     {
-        animator.SetBool("isAttack", false);
+
     }
     private void AttackTween()
     {
@@ -62,7 +66,7 @@ public class AttackState : IEnemyState
 
         seq.AppendCallback(() =>
         {
-            BackMotion();
+            isAttacking = false;
         });
     }
 
@@ -137,7 +141,6 @@ public class AttackState : IEnemyState
         // 회전 초기화
         avatar.rotation = Quaternion.identity;
 
-        isAttacking = false;
     }
 
 }
