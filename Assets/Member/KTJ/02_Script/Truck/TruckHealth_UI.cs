760f370c9fe49f397a3b3d1bf6d72ee153a7b8c3
Bimtaeur30 <Bimtaeur30@gmail.com>
2025-12-11 10:32:32 +0900

    TruckHealthSys

---
+++ Assets/Member/KTJ/02_Script/Truck/TruckHealth_UI.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TruckHealth_UI : MonoBehaviour
+{
+    [SerializeField] private Slider _healthBarSlider;
+    [SerializeField] private EventChannelSO_T<int> _onHealthChange;
+
+    private Coroutine _cor;
+
+    private void OnEnable()
+    {
+        _onHealthChange.OnEventRaised += UpdateHealth;
+    }
+
+    public void UpdateHealth(int currentH)
+    {
+        // 외부에서 이벤트로 부를 메서드 (단일 책임)
+        if (_cor != null) StopCoroutine(_cor);
+        _cor = StartCoroutine(HealthAnimation(_healthBarSlider.value, currentH, 0.2f));
+    }
+
+    private IEnumerator HealthAnimation(float start, float end, float duration)
+    {
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+
+            float t = elapsed / duration;
+            _healthBarSlider.value = Mathf.Lerp(start, end, t);
+
+            yield return null;
+        }
+
+        _healthBarSlider.value = end; // 마지막 값 보정
+    }
+}
