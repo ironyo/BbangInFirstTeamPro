d49fbe619f48e3b98e083cf85ab6844742c3c4fb
Valay0429 <a49986661@gmail.com>
2025-12-11 11:05:37 +0900

    money ui

--- Assets/Member/JJK/02.Scripts/UI/DigitSlot.cs
+++ Assets/Member/JJK/02.Scripts/UI/DigitSlot.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
@@ -8,25 +9,37 @@ public class DigitSlot : MonoBehaviour
     [SerializeField] private TextMeshProUGUI newDigit;
     [SerializeField] private float digitHeight = 60f;
     [SerializeField] private float tweenDuration = 0.2f;
+
+    public bool isDigit { get; set; } = false;
     
     private Tweener _fadeTween;
-    private Sequence _moveSeq;
+    private Sequence _rollSequence;
     
-    public void PlayChange(int from, int to)
+    public void PlayChange(int from, int to, bool plusDigit)
     {
         _fadeTween?.Kill();
+    
+        if (plusDigit)
+        {
+            oldDigit.text = from.ToString();
+            isDigit = true;
+        }
+        else
+        {
+            oldDigit.text = " ";
+            isDigit = true;
+        }
         
-        oldDigit.text = from.ToString();
         newDigit.text = to.ToString();
         newDigit.gameObject.SetActive(true);
         _fadeTween = oldDigit.DOFade(0, tweenDuration).SetEase(Ease.OutCubic);
-
+    
         newDigit.rectTransform.anchoredPosition = new Vector2(newDigit.rectTransform.anchoredPosition.x, -digitHeight);
         
-        _moveSeq = DOTween.Sequence();
+        Sequence _moveSeq = DOTween.Sequence();
         _moveSeq.Join(oldDigit.rectTransform.DOAnchorPosY(digitHeight, tweenDuration).SetEase(Ease.OutCubic));
         _moveSeq.Join(newDigit.rectTransform.DOAnchorPosY(0, tweenDuration).SetEase(Ease.OutCubic));
-
+    
         _moveSeq.OnComplete(() =>
         {
             // 최종 숫자 정리
@@ -38,14 +51,12 @@ public class DigitSlot : MonoBehaviour
         });
     }
     
-    public void SetInstant(int digit)
+    public void SetEmpty()
     {
-        oldDigit.text = digit.ToString();
-        oldDigit.alpha = 1;
-
-        oldDigit.rectTransform.anchoredPosition = new Vector2(oldDigit.rectTransform.anchoredPosition.x, 0);
-        newDigit.rectTransform.anchoredPosition = new Vector2(newDigit.rectTransform.anchoredPosition.x, -digitHeight);
-
+        oldDigit.text = "";
+        newDigit.text = "";
+        oldDigit.alpha = 0;
         newDigit.gameObject.SetActive(false);
+        isDigit = false;
     }
 }
