d5dbdf779f2fa388fb1bc608dcecf7a2be3f256a
ForRest1217 <Jihan2519@naver.com>
2025-12-13 01:09:58 +0900

    TurretSOPrefab

--- Assets/Member/CHG/02.Scripts/TestTurretCase.cs
+++ Assets/Member/CHG/02.Scripts/TestTurretCase.cs
@@ -5,26 +5,13 @@ namespace Assets.Member.CHG._02.Scripts
 {
     public class TestTurretCase : MonoBehaviour
     {
-        [SerializeField] private TurretSO _turretSO;
-        [SerializeField] private GunDataSO _gunDataSO;
+        [SerializeField] private GameObject turret;
         [SerializeField] private AffixSO _affixSO;
-        private GameObject obj;
 
         [ContextMenu("Start")]
         public void TestStart()
         {
-            if (_turretSO != null)
-            {
-                obj = Instantiate(_turretSO.TurretPrefab, transform.position, Quaternion.identity);
-                TurretBase turret = obj.GetComponent<TurretBase>();
-                turret.Init(_turretSO);
-            }
-            else if (_gunDataSO != null)
-            {
-                obj = Instantiate(_gunDataSO.TurretPrefab, transform.position, Quaternion.identity);
-                TurretBase turret = obj.GetComponent<TurretBase>();
-                turret.Init(_gunDataSO);
-            }
+            Instantiate(turret, transform.position, Quaternion.identity);
         }
     }
 }
