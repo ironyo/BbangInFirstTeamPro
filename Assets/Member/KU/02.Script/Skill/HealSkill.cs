98443e2694bfb25a601a14da858be9b39dd62cf7
ironyo <kyou09kang@gmail.com>
2025-12-14 01:49:35 +0900

    45ew3s4ertdgfh

--- Assets/Member/KU/02.Script/Skill/HealSkill.cs
+++ Assets/Member/KU/02.Script/Skill/HealSkill.cs
@@ -7,15 +7,18 @@ public class HealSkill : MonoBehaviour
     private int _currentCount = 0;
     [SerializeField] ItemDataSO _data;
 
+    private int _secHeal;
+
     private void Start()
     {
+        _secHeal = (int)_data.Value / (int)_data.Duration;
         StartCoroutine(HealCount());
     }
 
     private IEnumerator HealCount()
     {
         yield return new WaitForSeconds(1);
-        Debug.Log("회복메서드");
+        TruckHealthManager.Instance.TruckHealAmount(_secHeal);
         _currentCount++;
         if (_currentCount < _count)
             StartCoroutine(HealCount());
