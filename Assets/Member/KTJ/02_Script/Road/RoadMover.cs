5a49701dd8e9de40c0bfef9a9743f2db1114fdb9
ironyo <kyou09kang@gmail.com>
2025-12-14 21:28:43 +0900

    5t4erfds

--- Assets/Member/KTJ/02_Script/Road/RoadMover.cs
+++ Assets/Member/KTJ/02_Script/Road/RoadMover.cs
@@ -5,31 +5,53 @@ public class RoadMover : MonoBehaviour
     [SerializeField] private Transform[] _roads;
     [SerializeField] private RoadManager _manager;
 
-    private float _roadLength;
-    Vector3 leftPos;
+    private Camera _cam;
 
     private void Start()
     {
-        _roadLength = _roads[0].GetComponent<SpriteRenderer>().bounds.size.x;
+        _cam = Camera.main;
     }
 
     private void Update()
     {
         if (_manager.CurrentSpeed <= 0f) return;
 
-        float moveValue = _manager.CurrentSpeed * Time.deltaTime;
-
-        leftPos = Camera.main.ScreenToWorldPoint(
-        new Vector3(0f, Screen.height * 0.5f, Camera.main.nearClipPlane));
+        float move = _manager.CurrentSpeed * Time.deltaTime;
+        float leftEdge = _cam.ViewportToWorldPoint(new Vector3(0, 0.5f, 0)).x;
 
         foreach (var road in _roads)
         {
-            road.position += Vector3.left * moveValue;
+            road.position += Vector3.left * move;
 
-            if (road.position.x <= leftPos.x - _roadLength/2)
+            var roadRenderer = road.GetComponent<SpriteRenderer>();
+
+            const float SEAM_OVERLAP = 0.1f; // 0.01 ~ 0.03 사이 추천
+
+            if (roadRenderer.bounds.max.x < leftEdge)
             {
-                road.position += Vector3.right * _roadLength * _roads.Length;
+                SpriteRenderer rightMost = GetRightMostRoad();
+
+                float offset =
+                    rightMost.bounds.max.x - roadRenderer.bounds.min.x;
+
+                road.position += Vector3.right * (offset - SEAM_OVERLAP);
             }
+
         }
+    }
+
+    private SpriteRenderer GetRightMostRoad()
+    {
+        SpriteRenderer rightMost =
+            _roads[0].GetComponent<SpriteRenderer>();
+
+        foreach (var road in _roads)
+        {
+            var sr = road.GetComponent<SpriteRenderer>();
+            if (sr.bounds.max.x > rightMost.bounds.max.x)
+                rightMost = sr;
+        }
+
+        return rightMost;
     }
 }
