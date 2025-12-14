e829b69348e0544b38a7351754d5edef05fd6829
ironyo <kyou09kang@gmail.com>
2025-12-14 16:47:28 +0900

    123e3rtyhyu7drsrfzds

--- Assets/Member/KU/02.Script/Skill/HealSkill.cs
+++ Assets/Member/KU/02.Script/Skill/HealSkill.cs
@@ -1,8 +1,13 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class HealSkill : SlotSkillBase
 {
+    private List<TurretBase> turrets = new List<TurretBase>();
+
+    [SerializeField] GameObject _healParticle;
+
     private int _count = 5;
     private int _currentCount = 0;
     [SerializeField] ItemDataSO _data;
@@ -13,6 +18,12 @@ public class HealSkill : SlotSkillBase
     {
         _secHeal = (int)_data.Value / (int)_data.Duration;
         StartCoroutine(HealCount());
+
+        turrets.AddRange(Object.FindObjectsByType<TurretBase>(FindObjectsSortMode.None));
+        foreach (var item in turrets)
+        {
+            Instantiate(_healParticle, item.transform.position, Quaternion.identity);
+        }
     }
 
     private IEnumerator HealCount()
