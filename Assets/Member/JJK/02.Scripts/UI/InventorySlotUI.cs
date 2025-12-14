df8e5d4f454b769b2c83bf3157cac9fd837932d1
Valay0429 <a49986661@gmail.com>
2025-12-14 20:09:13 +0900

    money fix

--- Assets/Member/JJK/02.Scripts/UI/InventorySlotUI.cs
+++ Assets/Member/JJK/02.Scripts/UI/InventorySlotUI.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class InventorySlotUI : MonoBehaviour
 {
     [SerializeField] private Image icon;
+    [SerializeField] private Image backGroundIcon;
     private GameObject _skillInstance;
 
     public void Setup(ItemDataSO data)
@@ -13,6 +14,8 @@ public class InventorySlotUI : MonoBehaviour
         Clear();
         icon.enabled = true;
         icon.sprite = data.Icon;
+        backGroundIcon.enabled = true;
+        backGroundIcon.sprite = data.Icon;
         
         _skillInstance = Instantiate(data.Prefab, transform);
         var skill = _skillInstance.GetComponent<SlotSkillBase>();
@@ -22,6 +25,7 @@ public class InventorySlotUI : MonoBehaviour
     public void Clear()
     {
         icon.enabled = false;
+        backGroundIcon.enabled = false;
         icon.sprite = null;
 
         if (_skillInstance != null)
